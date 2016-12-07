b9b9669ca1de591174d3fbdbcd8c589aa786f1c7
SnpWebapp <revisit.webapp.test@gmail.com>
2016-12-07 22:04:10 +0100

    Update 0.0.1-admins.js

--- updates/0.0.1-admins.js
+++ updates/0.0.1-admins.js
@@ -9,7 +9,7 @@
 
 exports.create = {
 	User: [
-		{ 'name.first': 'Admin', 'name.last': 'User', 'email': 'revisit.webapp.test.gmail.com', 'password': 'admin', 'isAdmin': true },
+		{ 'name.first': 'Admin', 'name.last': 'User', 'email': 'revisit.webapp.test@gmail.com', 'password': 'admin', 'isAdmin': true },
 	],
 };
 
